fc403a9a46d4546b10eb0a5e1347483fd1aa3cd7
Dr. Johannes Brügmann <dr.johannes.bruegmann@gmail.com>
2025-02-21 11:53:28 +0100

    start implementing test generation

---
+++ hiking-client-example/src/test/java/erni/betterask/hiking/client/test/ArbitrarySupplierBuilder.java
@@ -0,0 +1,83 @@
+package erni.betterask.hiking.client.test;
+
+import io.swagger.v3.oas.models.media.Schema;
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ArbitrarySupplier;
+import net.jqwik.api.Builders;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+public class ArbitrarySupplierBuilder<T> implements ArbitrarySupplier<T> {
+
+    private final Model model;
+
+    public ArbitrarySupplierBuilder(@NotNull final String fname, @NotNull final String model) {
+        this.model = new Model(fname, model);
+        for (Map.Entry<String, Schema> entry : this.model.getSchema().getProperties().entrySet()) {
+            final String name = entry.getKey();
+            final Schema<?> schema = entry.getValue();
+            switch (schema.getType())
+        }
+    }
+
+    public static class ObjectSupplierBuilder<U> implements ArbitrarySupplier<U> {
+
+        private final Schema<?> schema;
+
+        public ObjectSupplierBuilder(@NotNull final Schema<?> schema) {
+            this.schema = schema;
+        }
+
+        @Override
+        public Arbitrary<U> get() {
+            return Builders.withBuilder().build();
+        }
+
+    }
+
+    public static class StringSupplierBuilder implements ArbitrarySupplier<String> {
+
+        private final Schema<?> schema;
+
+        public StringSupplierBuilder(@NotNull final Schema<?> schema) {
+            this.schema = schema;
+        }
+
+        public ArbitrarySupplier<String> build() {
+            return this;
+        }
+
+        @Override
+        public Arbitrary<String> get() {
+            return Arbitraries.strings()
+                    .ofMinLength(this.schema.getMinLength())
+                    .ofMaxLength(this.schema.getMaxLength())
+                    .injectNull(this.schema.getNullable() ? 0.1 : 0.0);
+        }
+
+    }
+
+    public static class IntegerSupplierBuilder implements ArbitrarySupplier<Integer> {
+
+        private final Schema<?> schema;
+
+        public IntegerSupplierBuilder(@NotNull final Schema<?> schema) {
+            this.schema = schema;
+        }
+
+        public ArbitrarySupplier<Integer> build() {
+            return this;
+        }
+
+        @Override
+        public Arbitrary<Integer> get() {
+            return Arbitraries.integers()
+                    .lessOrEqual(this.schema.getExclusiveMaximum() ? this.schema.getMaximum().intValue() - 1 : this.schema.getMaximum().intValue())
+                    .greaterOrEqual(this.schema.getExclusiveMinimum() ? this.schema.getMinimum().intValue() + 1 : this.schema.getMinimum().intValue())
+                    .injectNull(this.schema.getNullable() ? 0.1 : 0.0);
+        }
+    }
+
+}
---
+++ hiking-client-example/src/test/java/erni/betterask/hiking/client/test/Model.java
@@ -0,0 +1,54 @@
+package erni.betterask.hiking.client.test;
+
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import org.openapitools.codegen.InlineModelResolver;
+import org.openapitools.codegen.utils.ModelUtils;
+
+import javax.validation.constraints.NotNull;
+
+public class Model {
+
+    private final Schema<?> schema;
+
+    public Model(@NotNull final String openApiSpec, @NotNull final String model) {
+        this.schema = this.parseOpenApi(openApiSpec).getComponents().getSchemas().get(model);
+    }
+
+    public Schema<?> getSchema() {
+        return this.schema;
+    }
+
+    public boolean isArray() {
+        return this.matchesTypeName("array");
+    }
+
+    public boolean isObject() {
+        return this.matchesTypeName("object");
+    }
+
+    public boolean isInteger() {
+        return this.matchesTypeName("integer");
+    }
+
+    public boolean isString() {
+        return this.matchesTypeName("string");
+    }
+
+    private boolean matchesTypeName(final String typeName) {
+        final String type = this.schema.getType();
+        if (type == null || type.trim().isEmpty()) {
+            return false;
+        }
+        return typeName.equals(type);
+    }
+
+    private OpenAPI parseOpenApi(@NotNull final String openApiSpec) {
+        final OpenAPI openAPI = new OpenAPIParser().readLocation(openApiSpec, null, new ParseOptions()).getOpenAPI();
+        ModelUtils.getOpenApiVersion(openAPI, openApiSpec, null);
+        InlineModelResolver resolver = new InlineModelResolver();
+        return openAPI;
+    }
+}
