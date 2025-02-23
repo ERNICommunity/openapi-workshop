c71bc7f52e807618cb48c7fa3366644d0a61ec6e
Dr. Johannes Brügmann <dr.johannes.bruegmann@gmail.com>
2025-02-23 09:15:00 +0100

    - add array supplier for unique items (sets) - make array suppliers nullable if required

--- hiking-client-example/src/test/java/erni/betterask/openapi/resttemplate/test/UniqueItemsArraySupplier.java
+++ hiking-client-example/src/test/java/erni/betterask/openapi/resttemplate/test/UniqueItemsArraySupplier.java
@@ -5,24 +5,24 @@ import lombok.SneakyThrows;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.ArbitrarySupplier;
 
-import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
-public class ArraySupplier implements ArbitrarySupplier<List<Object>> {
+public class UniqueItemsArraySupplier implements ArbitrarySupplier<Set<Object>> {
 
     private final Schema<?> schema;
     private final Arbitrary<Object> itemArbitrary;
 
     @SneakyThrows
     @SuppressWarnings("unchecked")
-    public ArraySupplier(Schema<?> schema, Arbitrary<?> itemArbitrary) {
+    public UniqueItemsArraySupplier(Schema<?> schema, Arbitrary<?> itemArbitrary) {
         this.schema = schema;
         this.itemArbitrary = (Arbitrary<Object>) itemArbitrary;
     }
 
     @Override
-    public Arbitrary<List<Object>> get() {
-        return itemArbitrary.list()
+    public Arbitrary<Set<Object>> get() {
+        return itemArbitrary.set()
                 .ofMinSize(Objects.requireNonNullElse(schema.getMinItems(), 0))
                 .ofMaxSize(Math.min(Objects.requireNonNullElse(schema.getMaxItems(), Integer.MAX_VALUE), 99))
                 .injectNull(Objects.requireNonNullElse(schema.getNullable(), false) ? 0.1 : 0.0);
